bd58d59e00d489bc7a55b1e880fb410f70974cfc
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-19 15:05:24 +0200

    added random numbers generation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-alert()
+const toGuessEl = document.querySelector(".to_guess");
+console.log(toGuessEl);
+
+for (let i = 0; i < 5; i++) {
+    const numToGuessEl = document.createElement("div");
+    numToGuessEl.classList.add("num_to_guess");
+
+    const num = generateRandom(1, 100);
+    numToGuessEl.innerText = num;
+    console.log(num);
+    console.log(numToGuessEl);
+
+    toGuessEl.insertAdjacentElement("beforeend", numToGuessEl);
+}
+
+function generateRandom(min, max) {
+    return Math.floor(Math.random() * max + min);
+}
